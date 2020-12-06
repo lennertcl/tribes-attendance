77fd041a38124481745c627c10ea152e42286411
robbedeboosere <deboosererobbe2001@gmail.com>
2020-12-06 12:51:31 +0100

    vinkje fix + press 'q' to quit program properly

--- football.py
+++ football.py
@@ -96,13 +96,14 @@ def colnum_string(n):
     return string
 
 def getId():
-    # initalize the cam
     result = -1
+    #windows
     cap = cv2.VideoCapture(cv2.CAP_DSHOW)
+    #mac
+    #cap = cv2.VideoCapture(0)
     # initialize the cv2 QRCode detector
     detector = cv2.QRCodeDetector()
-    running = True
-    while running:
+    while True:
         _, img = cap.read()
         # detect and decode
         data, bbox, _ = detector.detectAndDecode(img)
@@ -113,20 +114,27 @@ def getId():
                 # draw all lines
                 cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i + 1) % len(bbox)][0]), color=(255, 0, 0), thickness=2)
             if data:
-                running = False
                 result = data
                 vinkje = cv2.imread("vinkje.jpg")
                 cv2.imshow("vink", vinkje)
                 cv2.waitKey(0)
-                time.sleep(2)
+                time.sleep(1)
+                break
         # display the result
         cv2.imshow("img", img)
         if cv2.waitKey(1) == ord("q"):
+            quit = True
             break
     cap.release()
     cv2.destroyAllWindows()
+    if quit:
+        return -1
     return result
 
 if __name__ == '__main__':
     while True:
-        updateValue(getId())
+        value = getId()
+        if value == -1:
+            break
+        else:
+            updateValue(value)
